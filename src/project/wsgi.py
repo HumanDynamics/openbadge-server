0aad25ac3f82c728b8f6c9cdbcd0121b15141106
Craig Ferguson <craigsonoffergus@gmail.com>
2016-05-16 15:00:23 -0400

    fixd up some small things with the deployment code

--- src/project/wsgi.py
+++ src/project/wsgi.py
@@ -7,7 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+sys.path.append("/opt")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 from django.core.wsgi import get_wsgi_application
