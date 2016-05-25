3a1f75d19c8e2530e7f14a3c5ce3f5866d3e1061
Oren Lederman <oren.lederman@gmail.com>
2016-05-24 22:09:55 -0400

    simple test for calling analysis commands

--- src/openbadge/tests.py
+++ src/openbadge/tests.py
@@ -8,8 +8,43 @@ from django.db import transaction
 
 from rest_framework_expiring_authtoken.models import ExpiringToken
 
+from .models import StudyGroup, StudyMember, Meeting
+import analysis
+
 class APITest(TestCase):
 
     def setUp(self):
-        pass
+        # setup group, members, meeting
+        StudyGroup.objects.create(name="Oren group")
+        group = StudyGroup.objects.get(name="Oren group")
 
+        StudyMember.objects.create(name="Oren Lederman", email = "badgeA@or3n.com", badge="AA:AA:AA:AA", group = group)
+        StudyMember.objects.create(name="Alice", email="badgeB@or3n.com", badge="AA:AA:AA:AA", group=group)
+        member1 = StudyMember.objects.get(name="Oren Lederman")
+        member2 = StudyMember.objects.get(name="Alice")
+
+        memberKeys = [member1.key,member2.key]
+        meetingMembers = simplejson.dumps(memberKeys)
+
+        Meeting.objects.create(\
+            group = group, uuid="fakeid",type="fake_type",location="fake_location",description="fake description"\
+            ,is_complete=True,show_visualization=True,start_time=datetime.datetime.now(),end_time=datetime.datetime.now()\
+            ,members=meetingMembers,log_file="logs/5YTKC/5YTKC_1464127859263.txt"
+            )
+
+    def test_group_created(self):
+        group = StudyGroup.objects.get(name="Oren group")
+        members = group.members.all()
+        print("The group is :",group)
+        names = [member.name for member in members]
+        print("Member names :",names)
+        meetings = meetings = group.meetings.all()
+        print("Meetings: ",meetings)
+        print("Meeting members: ", meetings[0].members)
+
+    def test_group_send_survey(self):
+        group = StudyGroup.objects.get(name="Oren group")
+        meetings = group.meetings.all()
+        meeting = meetings[0]
+
+        analysis.post_meeting_analysis(meeting)
