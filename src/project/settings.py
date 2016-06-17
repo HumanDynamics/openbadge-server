0ceacd611cbbe722da76370e6a43c1bb2ecbfd6b
cyntzhou <cyntzhou@gmail.com>
2016-06-17 14:10:13 -0400

    getting ready for merge to master

--- src/project/settings.py
+++ src/project/settings.py
@@ -197,8 +197,8 @@ STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "../staticfiles")
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "../media")
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "../media")
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
