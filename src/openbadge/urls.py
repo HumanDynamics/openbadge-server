93740e464bd029d0aac8b770f547f9b7c43c33f5
Oren Lederman <oren.lederman@gmail.com>
2016-06-19 17:02:46 -0400

    Adding urls and views that were removed by mistake

--- src/openbadge/urls.py
+++ src/openbadge/urls.py
@@ -1,24 +1,14 @@
-from django.conf.urls import include, url, patterns
-from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls import include, url
 from . import views
 from django.views.generic.base import TemplateView
 
-from django.conf import settings
-from django.conf.urls.static import static
-
 urlpatterns = [
+    # APP URLs. DO NOT TOUCH THESE
     url(r'^get_group/(?P<group_key>\w+)/$', views.get_group, name='get_group'),
     url(r'^get_finished_meetings/(?P<group_key>\w+)/$', views.get_finished_meetings, name='get_finished_meetings'),
     url(r'^log_data/$', views.log_data, name='log_data'),
 
+    # Reports
     url(r'^internal_report/$', views.internal_report, name='internal_report'),
     url(r'^weekly_group_report/(?P<group_key>\w+)/(?P<week_num>[0-9]+)$', views.weekly_group_report, name='weekly_group_report'),
 ]
-
-## debug stuff to serve static media
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^media/(?P<path>.*)$', 'django.views.static.serve', 
-            {'document_root': settings.MEDIA_ROOT}),
-   )
