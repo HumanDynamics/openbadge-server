cf9a1f00d2b735c99954e8d6629a7ca2a076cfae
Oren Lederman <oren.lederman@gmail.com>
2016-06-19 16:01:56 -0400

    Added urls that were removed by mistake

--- src/openbadge/urls.py
+++ src/openbadge/urls.py
@@ -8,7 +8,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    url(r'^get_group/(?P<group_key>\w+)/$', views.get_group, name='get_group'),
+    url(r'^get_finished_meetings/(?P<group_key>\w+)/$', views.get_finished_meetings, name='get_finished_meetings'),
     url(r'^log_data/$', views.log_data, name='log_data'),
+
     url(r'^internal_report/$', views.internal_report, name='internal_report'),
     url(r'^weekly_group_report/(?P<group_key>\w+)/(?P<week_num>[0-9]+)$', views.weekly_group_report, name='weekly_group_report'),
 ]
