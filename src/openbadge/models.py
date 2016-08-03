ad2395bfe830f257e54b41c46c82c1ceb300f44b
Jackson Kearl <jkearl@mit.edu>
2016-08-03 11:29:15 -0400

    convert to timestamps

--- src/openbadge/models.py
+++ src/openbadge/models.py
@@ -185,13 +185,13 @@ class Meeting(BaseModel):
     uuid = models.CharField(max_length=64, db_index=True, unique=True)
     """this will be something the phone can generate and give us, like [hub_uuid]-[start_time]"""
 
-    start_time = models.FloatField(null=True, blank=True)
+    start_time = models.DecimalField(decimal_places=3, max_digits= 20, null=True)
     """time they hit start"""
 
-    end_time = models.FloatField(null=True, blank=True)
+    end_time = models.DecimalField(decimal_places=3, max_digits= 20, null=True)
     """time that they either hit end, or that the meeting timesout."""
 
-    last_update_timestamp = models.FloatField(null=True, blank=True)
+    last_update_timestamp = models.DecimalField(decimal_places=3, max_digits= 20, null=True)
     """log_timestamp of the last chunk received"""
 
     last_update_index = models.IntegerField(null=True, blank=True)
