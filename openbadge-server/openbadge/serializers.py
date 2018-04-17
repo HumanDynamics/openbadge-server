d3347bdce5673d6d6dc23c3e8c17a296de4d8e5c
Oren Lederman <oren.lederman@gmail.com>
2018-04-16 20:55:23 -0400

    Fixing typo in advertisement

--- openbadge-server/openbadge/serializers.py
+++ openbadge-server/openbadge/serializers.py
@@ -9,14 +9,14 @@ logger = logging.getLogger(__name__)
 
 
 class MemberSerializer(serializers.ModelSerializer):
-    advertisment_project_id = serializers.ReadOnlyField(source='get_advertisment_project_id')
+    advertisement_project_id = serializers.ReadOnlyField(source='get_advertisement_project_id')
     project = serializers.PrimaryKeyRelatedField(queryset=Project.objects.all())
 
     class Meta:
         model = Member
-        fields = ('id', 'project', 'advertisment_project_id', 'name', 'email', 'badge', 'observed_id', 'active', 'comments','last_seen_ts', 'last_audio_ts',
+        fields = ('id', 'project', 'advertisement_project_id', 'name', 'email', 'badge', 'observed_id', 'active', 'comments','last_seen_ts', 'last_audio_ts',
                   'last_audio_ts_fract', 'last_proximity_ts', 'last_contacted_ts', 'last_unsync_ts', 'last_voltage', 'key')
-        read_only_fields = ('id','project','advertisment_project_id', 'key')
+        read_only_fields = ('id','project','advertisement_project_id', 'key')
 
     def update(self, instance, validated_data):
 
@@ -51,14 +51,14 @@ class MemberSerializer(serializers.ModelSerializer):
 
 
 class BeaconSerializer(serializers.ModelSerializer):
-    advertisment_project_id = serializers.ReadOnlyField(source='get_advertisment_project_id')
+    advertisement_project_id = serializers.ReadOnlyField(source='get_advertisement_project_id')
     project = serializers.PrimaryKeyRelatedField(queryset=Project.objects.all())
     
     class Meta:
         model = Beacon
-        fields = ('id','project', 'advertisment_project_id','name', 'badge', 'observed_id','active', 'comments',
+        fields = ('id','project', 'advertisement_project_id','name', 'badge', 'observed_id','active', 'comments',
          'last_seen_ts','last_voltage', 'key')
-        read_only_fields = ('advertisment_project_id', 'id','project', 'key')
+        read_only_fields = ('advertisement_project_id', 'id','project', 'key')
 
     def update(self, instance, validated_data):
         # if we have an older last_seen_ts, update it and voltage
