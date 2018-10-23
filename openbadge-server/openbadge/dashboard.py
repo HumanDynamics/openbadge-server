ba3d0df2f29002b84461d533d791b7cc434953a2
Jordan Reedie <jjreedie@gmail.com>
2018-10-23 15:19:16 -0400

    fix bug where members weren't showing up in dash
    Setting queryset & cutoff_ts as class variables
    would cause the cutoff timestamps to be calculated only once
    Using get_queryset calculates timestamp cutoff
    at time of execution instead

--- openbadge-server/openbadge/dashboard.py
+++ openbadge-server/openbadge/dashboard.py
@@ -1,6 +1,6 @@
 from controlcenter import Dashboard, widgets
 from .models import Member, Unsync, Hub, Beacon
-from django.conf import settings 
+from django.conf import settings
 from django.db.models import Count
 import time
 
@@ -18,16 +18,20 @@ def cutoff_to_ts(cutoff):
     # cutoff is in hours
     return time.time() - hours_to_secs(cutoff)
 
-# TODO filter by project??
-
 class LowVoltageMembers(widgets.ItemList):
+
     model = Member
-    queryset = model.objects.filter(active=True).filter(last_voltage__lt=settings.LOW_VOLTAGE).order_by('last_voltage')
     list_display = ('id', 'key', 'last_seen_ts', 'last_voltage', 'last_unsync_ts')
     width = widgets.LARGE
     sortable = True
     limit_to = None
     height = 400
+
+    def get_queryset(self):
+        return (self.model.objects
+                .filter(active=True)
+                .filter(last_voltage__lt=settings.LOW_VOLTAGE)
+                .order_by('last_voltage'))
 
 
 class ManyResetMembers(widgets.ItemList):
@@ -39,15 +43,15 @@ class ManyResetMembers(widgets.ItemList):
     # we need all members with > x number of unsyncs since y
     # width = widgets.FULL
     sortable = True
-    
-    cutoff_ts = cutoff_to_ts(settings.UNSYNC_CUTOFF_HOURS)
+
     # get all unsyncs since cutoff
-    queryset = (model.objects
-            .filter(unsync_ts__gt=cutoff_ts)
-            .values('member__id', 'member__key', 'member__name', 'member__last_voltage')
-            .annotate(num_unsyncs=Count('member__id'))
-            .filter(num_unsyncs__gte=settings.NUM_UNSYNCS)
-            .order_by('-num_unsyncs'))
+    def get_queryset(self):
+        return (self.model.objects
+                .filter(unsync_ts__gt=cutoff_to_ts(settings.UNSYNC_CUTOFF_HOURS))
+                .values('member__id', 'member__key', 'member__name', 'member__last_voltage')
+                .annotate(num_unsyncs=Count('member__id'))
+                .filter(num_unsyncs__gte=settings.NUM_UNSYNCS)
+                .order_by('-num_unsyncs'))
 
     list_display = ('member__id', 'member__key', 'member__name', 'num_unsyncs', 'member__last_voltage')
 
@@ -63,47 +67,67 @@ class ThingNotSeen(widgets.ItemList):
         else:
             return "Not yet seen"
 
+    def cutoff_long(self):
+        return cutoff_to_ts(settings.LAST_SEEN_CUTOFF_LONG_HOURS)
+
+    def cutoff_short(self):
+        return cutoff_to_ts(settings.LAST_SEEN_CUTOFF_SHORT_HOURS)
+
     minutes_since_last_seen.short_description = "minutes since last seen"
     minutes_since_last_seen.allow_tags = True
 
 class HubsNotSeen(ThingNotSeen):
     model = Hub
     title = "HUBS NOT SEEN IN {} HOURS".format(settings.LAST_SEEN_CUTOFF_SHORT_HOURS)
-    cutoff_ts = cutoff_to_ts(settings.LAST_SEEN_CUTOFF_SHORT_HOURS)
-    queryset = model.objects.filter(last_seen_ts__lt=cutoff_ts)
     list_display = ('id', 'key', 'last_seen_ts', 'minutes_since_last_seen')
+
+    def get_queryset(self):
+        return self.model.objects.filter(last_seen_ts__lt=self.cutoff_short())
 
 
 class BeaconsNotSeen(ThingNotSeen):
     model = Beacon
     title = "BEACONS NOT SEEN IN {} HOURS".format(settings.LAST_SEEN_CUTOFF_SHORT_HOURS)
-    cutoff_ts = cutoff_to_ts(settings.LAST_SEEN_CUTOFF_SHORT_HOURS)
-    queryset = model.objects.filter(last_seen_ts__lt=cutoff_ts).filter(active=True).order_by('last_seen_ts')
     list_display = ('id', 'key', 'last_seen_ts', 'last_voltage', 'minutes_since_last_seen')
+
+    def get_queryset(self):
+        return (self.model.objects
+                .filter(last_seen_ts__lt=self.cutoff_short())
+                .filter(active=True)
+                .order_by('last_seen_ts'))
 
 
 class MembersNotSeenShort(ThingNotSeen):
     model = Member
     title = "MEMBERS NOT SEEN IN {} HOURS".format(settings.LAST_SEEN_CUTOFF_SHORT_HOURS)
-    # cutoff is in hours
-    cutoff_ts = cutoff_to_ts(settings.LAST_SEEN_CUTOFF_SHORT_HOURS)
-    queryset = model.objects.filter(last_seen_ts__lt=cutoff_ts).filter(active=True).order_by('last_seen_ts')
     list_display = ('id', 'key', 'last_seen_ts', 'minutes_since_last_seen', 'last_voltage', 'last_unsync_ts')
+
+    def get_queryset(self):
+        return (self.model.objects
+                .filter(last_seen_ts__lt=self.cutoff_short())
+                .filter(active=True)
+                .order_by('last_seen_ts'))
 
 
 class MembersNotSeenLong(ThingNotSeen):
     model = Member
     title = "MEMBERS NOT SEEN IN {} HOURS".format(settings.LAST_SEEN_CUTOFF_LONG_HOURS)
-    cutoff_ts = cutoff_to_ts(settings.LAST_SEEN_CUTOFF_LONG_HOURS)
-    queryset = model.objects.filter(last_seen_ts__lt=cutoff_ts).filter(active=True).order_by('last_seen_ts')
     list_display = ('id', 'key', 'last_seen_ts', 'minutes_since_last_seen', 'last_voltage', 'last_unsync_ts')
+
+    def get_queryset(self):
+        return (self.model.objects
+                .filter(last_seen_ts__lt=self.cutoff_long())
+                .filter(active=True)
+                .order_by('last_seen_ts'))
+
 
 class MembersAll(ThingNotSeen):
     model = Member
     title = "ALL MEMBERS"
-    queryset = model.objects.filter(active=True)
-    list_display = ('id', 'key', 'last_seen_ts',  'last_voltage', 'last_unsync_ts', 'minutes_since_last_seen')
+    list_display = ('id', 'key', 'last_seen_ts', 'minutes_since_last_seen', 'last_voltage', 'last_unsync_ts')
 
+    def get_queryset(self):
+        return self.model.objects.filter(active=True)
 
 
 class BadgeDashboard(Dashboard):
